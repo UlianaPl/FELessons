5026f9c595f48cc04a3f2371a26adf4f837eb1a0
UlianaPl <85ulyana@gmail.com>
2022-11-07 00:56:46 +0200

    index.html changes

---
+++ 28/assets/script.js
@@ -0,0 +1,38 @@
+let userName = prompt("What is your name?")
+alert(`Hello ${userName}`)
+
+console.log(userName)
+
+let userYear = prompt("When were you born?")
+const mainYear = 2022
+userAge = mainYear - userYear
+alert(`You are ${userAge}`)
+
+console.log(userYear, mainYear, userAge)
+
+let userLength = prompt("What is the square's side length?")
+squarePerimeter = userLength * 4
+alert(`The perimeter of the square is ${squarePerimeter}`)
+
+console.log(squarePerimeter, userLength)
+
+let circleRadius = prompt("What is the circle's radius?")
+circleArea = 3.14 * (circleRadius**2)
+alert(`The area of the circle is ${circleArea}`)
+
+console.log(squarePerimeter, userLength)
+
+let cityDistance = prompt("What is the distance between Lviv and Kyiv?")
+tripDuration = prompt("How many hours do you need to drive from Lviv to Kyiv?")
+carSpeed  = cityDistance / tripDuration
+alert(`Your speed should be ${carSpeed} km per one hour to get to Kyiv in time`)
+
+console.log(squarePerimeter, userLength)
+    
+let number1 = 0.1,
+    number2 = 0.2;
+console.log(number1 + number2)
+
+let item1 = '1',
+    item2 = 2;
+console.log(item1 + item2)
