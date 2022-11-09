77d5bc1edd5edb8f0d18a8fc78892ed71d840605
UlianaPl <85ulyana@gmail.com>
2022-11-10 01:37:21 +0200

    index.html changes

--- 28/assets/script.js
+++ 28/assets/script.js
@@ -1,4 +1,4 @@
-let userName = prompt("What is your name?")
+/*let userName = prompt("What is your name?")
 alert(`Hello ${userName}`)
 
 console.log(userName)
@@ -20,19 +20,66 @@ let circleRadius = prompt("What is the circle's radius?")
 circleArea = 3.14 * (circleRadius**2)
 alert(`The area of the circle is ${circleArea}`)
 
-console.log(squarePerimeter, userLength)
+console.log(circleRadius, circleArea)
 
 let cityDistance = prompt("What is the distance between Lviv and Kyiv?")
 tripDuration = prompt("How many hours do you need to drive from Lviv to Kyiv?")
 carSpeed  = cityDistance / tripDuration
 alert(`Your speed should be ${carSpeed} km per one hour to get to Kyiv in time`)
 
-console.log(squarePerimeter, userLength)
+console.log(cityDistance, tripDuration, carSpeed)*/
+
+let dollarCount = prompt("How many dollars do you want to exchange for euros?")
+const exchangeRate = 1
+euroCount = dollarCount * exchangeRate
+alert(`You wiil get ${euroCount} euros`)
+
+console.log(exchangeRate, dollarCount, euroCount)
+
     
 let number1 = 0.1,
     number2 = 0.2;
-console.log(number1 + number2)
+//console.log((number1 * 10 + number2 * 10) / 10)
+console.log((number1 + number2).toFixed(1))
 
 let item1 = '1',
     item2 = 2;
 console.log(item1 + item2)
+
+/*let flashdriveSize = prompt("What is your flash drive size?")
+    oneFile = 820
+    howMany = (flashdriveSize / oneFile).toFixed(2)
+alert(`Your flash drive has ${howMany} files, 820 Mb each`)
+      
+console.log(flashdriveSize, oneFile, howMany)*/
+
+/*let howMuch = prompt("How much money do you have in your wallet?")
+oneChocolate = 50
+confirm("One chocolate costs 50 UAH")
+howMany = (howMuch / oneChocolate).toFixed(1)
+alert(`You can buy ${howMany} chocolates`)
+      
+console.log(howMuch, oneChocolate, howMany)*/
+
+/*let countName = prompt("Please name a three-digit number")
+countName = [...countName].reverse().join("");
+ 
+alert(`Here is your three-digit number backwards - ${countName}`)
+      
+console.log(countName)*/
+
+/*let howMany = prompt("How much money do you want to deposit with a rate of 5% per year?")
+    howLong = 2
+    sumPercent = ((howMany * 5 /100) / 12 * howLong).toFixed(2)
+alert(`The amount of accrued interest on the deposit for 2 months is ${sumPercent}`)
+      
+console.log(sumPercent)*/
+
+2 && 0 && 3
+
+2 || 0 || 3
+
+2 && 0 || 3
+
+
+
