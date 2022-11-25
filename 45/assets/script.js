f303e20081202b2fbdd665db8a28c123a380bdb6
UlianaPl <85ulyana@gmail.com>
2022-11-25 08:09:53 +0200

    index.html changes

--- 45/assets/script.js
+++ 45/assets/script.js
@@ -1,6 +1,6 @@
 let scroll = document.getElementById('scroll')
 if (scroll) {
-    scroll.eventListener('click',scrollToElementFunc)
+    scroll.addeventListener('click',scrollToElementFunc)
 }
 function scrollToElementFunc() {
     let sectionTitle = document.querySelector('.section1__title');
@@ -11,3 +11,61 @@ function scrollToElementFunc() {
         })
     }
 }
+let aboutUs = document.getElementById('about_us')
+if (aboutUs) {
+    aboutUs.addeventListener('click',scrollToElementFunc)
+}
+function scrollToElementFunc() {
+    let sectionTitle = document.querySelector('.header__title');
+    if (sectionTitle) {
+        sectionTitle.scrollIntoView({
+            block: 'start',
+            behavior: 'smooth'
+        })
+    }
+}
+let projects = document.getElementById('projects')
+if (projects) {
+    projects.addeventListener('click',scrollToElementFunc)
+}
+function scrollToElementFunc() {
+    let sectionTitle = document.querySelector('.section1__title');
+    if (sectionTitle) {
+        sectionTitle.scrollIntoView({
+            block: 'start',
+            behavior: 'smooth'
+        })
+    }
+}
+let news = document.getElementById('news')
+if (news) {
+    news.addeventListener('click',scrollToElementFunc)
+}
+function scrollToElementFunc() {
+    let sectionTitle = document.querySelector('.section2__wrapper_title');
+    if (sectionTitle) {
+        sectionTitle.scrollIntoView({
+            block: 'start',
+            behavior: 'smooth'
+        })
+    }
+}
+let contact = document.getElementById('contact')
+if (contact) {
+    contact.addeventListener('click',scrollToElementFunc)
+}
+function scrollToElementFunc() {
+    let sectionTitle = document.querySelector('.form__title');
+    if (sectionTitle) {
+        sectionTitle.scrollIntoView({
+            block: 'start',
+            behavior: 'smooth'
+        })
+    }
+}
+$('.header__title').slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,    
+})
