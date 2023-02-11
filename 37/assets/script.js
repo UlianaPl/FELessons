893e6c3b8184010549a3885c832d36312d3303e9
UlianaPl <85ulyana@gmail.com>
2023-02-11 22:18:51 +0100

    index.html changes

---
+++ 37/assets/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(".header__list").click(function () {
+        $(this)
+            .addClass("active")
+            .removeClass("disable");
+        $(this)
+            .siblings()
+            .removeClass("active")
+            .addClass("disable");
+        $(".tab")
+            .css("display", "none")
+            .eq($(this).index())
+            .css("display", "block");
+    });
+});
