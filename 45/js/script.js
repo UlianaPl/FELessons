5f5e0284e25a89584909464651ad1731a3dd4597
UlianaPl <85ulyana@gmail.com>
2023-07-01 13:52:25 +0200

    Changes new

--- 45/js/script.js
+++ 45/js/script.js
@@ -55,72 +55,72 @@ Fancybox.bind('[data-fancybox="gallery"]', {
        // Опції FancyBox, якщо необхідно
      });
    });
-
-// let scroll = document.getElementById('scroll')
-// if (scroll) {
-//     scroll.addEventListener('click',scrollToFunc)
-// }
-// function scrollToFunc() {
-//     let sectionTitle = document.querySelector('.section1');
-//     if (sectionTitle) {
-//         sectionTitle.scrollIntoView({
-//             block: 'start',
-//             behavior: 'smooth'
-//         })
-//     }
-// }
-// let aboutUs = document.getElementById('about_us')
-// if (aboutUs) {
-//     aboutUs.addEventListener('click',scrollElementFunc)
-// }
-// function scrollElementFunc() {
-//     let sectionTitle = document.querySelector('.header__title');
-//     if (sectionTitle) {
-//         sectionTitle.scrollIntoView({
-//             block: 'start',
-//             behavior: 'smooth'
-//         })
-//     }
-// }
-// let projects = document.getElementById('projects')
-// if (projects) {
-//     projects.addEventListener('click',scrollFunc)
-// }
-// function scrollFunc() {
-//     let sectionTitle = document.querySelector('.section1__container');
-//     if (sectionTitle) {
-//         sectionTitle.scrollIntoView({
-//             block: 'start',
-//             behavior: 'smooth'
-//         })
-//     }
-// }
-// let news = document.getElementById('news')
-// if (news) {
-//     news.addEventListener('click',toElementFunc)
-// }
-// function toElementFunc() {
-//     let sectionTitle = document.querySelector('.section2__container');
-//     if (sectionTitle) {
-//         sectionTitle.scrollIntoView({
-//             block: 'start',
-//             behavior: 'smooth'
-//         })
-//     }
-// }
-// let contact = document.getElementById('contact')
-// if (contact) {
-//     contact.addEventListener('click',scrollFunc)
-// }
-// function scrollFunc() {
-//     let sectionTitle = document.querySelector('.section4__container');
-//     if (sectionTitle) {
-//         sectionTitle.scrollIntoView({
-//             block: 'start',
-//             behavior: 'smooth'
-//         })
-//     }
-// }
+*/
+let scroll = document.getElementById('scroll')
+if (scroll) {
+    scroll.addEventListener('click',scrollToFunc)
+}
+function scrollToFunc() {
+    let sectionTitle = document.querySelector('.section1');
+    if (sectionTitle) {
+        sectionTitle.scrollIntoView({
+            block: 'start',
+            behavior: 'smooth'
+        })
+    }
+}
+let aboutUs = document.getElementById('about_us')
+if (aboutUs) {
+    aboutUs.addEventListener('click',scrollElementFunc)
+}
+function scrollElementFunc() {
+    let sectionTitle = document.querySelector('.section1');
+    if (sectionTitle) {
+        sectionTitle.scrollIntoView({
+            block: 'start',
+            behavior: 'smooth'
+        })
+    }
+}
+let projects = document.getElementById('projects')
+if (projects) {
+    projects.addEventListener('click',scrollFunc)
+}
+function scrollFunc() {
+    let sectionTitle = document.querySelector('.section1__container');
+    if (sectionTitle) {
+        sectionTitle.scrollIntoView({
+            block: 'start',
+            behavior: 'smooth'
+        })
+    }
+}
+let news = document.getElementById('news')
+if (news) {
+    news.addEventListener('click',toElementFunc)
+}
+function toElementFunc() {
+    let sectionTitle = document.querySelector('.section2__container');
+    if (sectionTitle) {
+        sectionTitle.scrollIntoView({
+            block: 'start',
+            behavior: 'smooth'
+        })
+    }
+}
+let contact = document.getElementById('contact')
+if (contact) {
+    contact.addEventListener('click',scrollFunc)
+}
+function scrollFunc() {
+    let sectionTitle = document.querySelector('.section4__container');
+    if (sectionTitle) {
+        sectionTitle.scrollIntoView({
+            block: 'start',
+            behavior: 'smooth'
+        })
+    }
+}
 //
 //
 // $(function() {
@@ -134,25 +134,25 @@ Fancybox.bind('[data-fancybox="gallery"]', {
 //     })
 // })
 //
-// function initMap() {
-//     // Координати центру карти
-//     const center = { lat: 51.5074, lng: -0.1278 };
-//
-//     // Створення карти
-//     const map = new google.maps.Map(document.getElementById("map"), {
-//       center: center,
-//       zoom: 12,
-//     });
-//
-//     // Створення кастомного маркера
-//     const marker = new google.maps.Marker({
-//       position: center,
-//       map: map,
-//       icon: "path/to/custom-marker.png", // Шлях до зображення кастомного маркера
-//       draggable: true, // Якщо ви хочете, щоб маркер був пересуваним
-//     });
-//   }
-//
+/*function initMap() {
+    // Координати центру карти
+    const center = { lat: 49.842957, lng: 24.031111 };
+
+    // Створення карти
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: center,
+      zoom: 12,
+    });
+
+    // Створення кастомного маркера
+    const marker = new google.maps.Marker({
+      position: center,
+      map: map,
+      icon: "path/to/custom-marker.png", // Шлях до зображення кастомного маркера
+      draggable: true, // Якщо ви хочете, щоб маркер був пересуваним
+    });
+  }
+
 //   const image = document.querySelector('.section3__wrapper_one');
 //
 //   image.addEventListener('mouseenter', function() {
@@ -172,28 +172,16 @@ Fancybox.bind('[data-fancybox="gallery"]', {
 //     targetElement.scrollIntoView({ behavior: 'smooth' });
 //   }
 //
-// AIzaSyBN9XcuV-uz8KNK8lOg-r9lDuFXD7mS1Rs
-// $(document).ready(function() {
-//     $('.multiple__slider').slick({
-//         infinite: true,
-//         arrows: true,
-//         dots: true,
-//         slidesToShow: 3,
-//         slidesToScroll: 3,
-//
-//     })
-// })
-//
 // setInterval(function () {
 //     $('.slickPrev').slick("slickPrev");
-// }, 2000);
-// function initMap() {
-//     new google.maps.Map(document.getElementById("map"), {
-//       mapId: "8e0a97af9386fef",
-//       center: { lat: 48.85, lng: 2.35 },
-//       zoom: 12,
-//     });
-//   }
-//
-//   window.initMap = initMap;
-//   */
+// }, 2000);*/
+function initMap() {
+    new google.maps.Map(document.getElementById("map"), {
+      mapId: "8e0a97af9386fef",
+      center: { lat: 49.842957, lng: 24.031111 },
+      zoom: 12,
+    });
+  }
+
+  window.initMap = initMap;
+  
