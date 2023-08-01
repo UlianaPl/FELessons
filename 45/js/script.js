d194b4e6c26f5a6f4a1d67c567d572569277cb61
Your Name <you@example.com>
2023-08-01 21:48:51 +0300

    project 2

--- 45/js/script.js
+++ 45/js/script.js
@@ -1,195 +1,140 @@
-// Burger menu
-$('.burger-menu').on('click', function (e){
-    $('.burger-menu').toggleClass('active');
-    $('header').toggleClass('active');
-})
+//Header scroll sticky
 
-const burgerMenu = document.querySelector('.burger-menu');
-const menu = document.querySelector('.main-menu ul');
-
-burgerMenu.addEventListener('click', () => {
-  burgerMenu.classList.toggle('active');
-  menu.classList.toggle('active');
+let element = document.querySelector('.header');
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 53) {
+    element.classList.add("header__sticky");
+  } else {
+    element.classList.remove("header__sticky");
+  }
 });
 
-// Slick slider
-$(function() {
-    $('.main__slider .top-slider').slick({
-        infinite: true,
-        arrows: false,
-        dots: true,
-        vertical: true,
-        verticalSwiping: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    })
-})
+//Active menu 
 
-//Swiper slider
-const swiper = new Swiper('.news-slider', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        20: {
-            slidesPerView: 1,
-        },
-        500: {
-            slidesPerView: 2,
-        },
-        900: {
-            slidesPerView: 3,
-        }
+let section = document.querySelectorAll('section');
+let navLink = document.querySelectorAll('header nav a');
+window.onscroll = () => {
+  section.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop-150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+    if(top > offset && top < offset + height){
+      navLink.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
     }
+  });
+};
+
+//Burger menu
+
+  document.querySelector('.header__burger').addEventListener('click',function(event) {
+    document.querySelector('.header__burger,.header__menu').classList.toggle('active');
+    document.querySelector('body').classList.toggle('lock');
+  });
+
+// Smooth scrolling to the anchor
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    let href = this.getAttribute('href').substring(1);
+    const scrollTarget = document.getElementById(href);
+    const topOffset = 70;
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth'
+    });
+  });
 });
+
+//Slider for the section main_screen
+
+const swiper = new Swiper(".main_screen__slider", {
+  direction: "vertical",
+  spaceBetween: 30,
+  effect: "flip",
+  grabCursor: true,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+//Slider for the section news 
+
+const swiperNews = new Swiper(".news__slider", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  }
+});
+
+//Fancybox
 
 Fancybox.bind('[data-fancybox="gallery"]', {
-    on: {
-      done: (fancybox) => {
-        // here `fancybox` refers to the current instance
-      },
+});
+
+// Google Map
+
+let map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("googleMap"), {
+    center: { lat: 40.6686, lng: -73.8999 },
+    zoom: 13.5,
+    mapId: '2f084b4895741534',
+  });
+  const marker = new google.maps.Marker({
+    position: { lat: 40.6781, lng: -73.8981 },
+    map: map,
+    title: "Monticello",
+    icon: {
+      url: "https://i.postimg.cc/30jd33nS/Pin.png",
+      scaledSize: new google.maps.Size(100, 100),
     },
   });
-/*$(document).ready(function() {
-     $("[data-fancybox-trigger='gallery']").fancybox({
-       // Опції FancyBox, якщо необхідно
-     });
-   });
-*/
-let scroll = document.getElementById('scroll')
-if (scroll) {
-    scroll.addEventListener('click',scrollToFunc)
 }
-function scrollToFunc() {
-    let sectionTitle = document.querySelector('.section1');
-    if (sectionTitle) {
-        sectionTitle.scrollIntoView({
-            block: 'start',
-            behavior: 'smooth'
-        })
-    }
-}
-let aboutUs = document.getElementById('about_us')
-if (aboutUs) {
-    aboutUs.addEventListener('click',scrollElementFunc)
-}
-function scrollElementFunc() {
-    let sectionTitle = document.querySelector('.section1');
-    if (sectionTitle) {
-        sectionTitle.scrollIntoView({
-            block: 'start',
-            behavior: 'smooth'
-        })
-    }
-}
-let projects = document.getElementById('projects')
-if (projects) {
-    projects.addEventListener('click',scrollFunc)
-}
-function scrollFunc() {
-    let sectionTitle = document.querySelector('.section1__container');
-    if (sectionTitle) {
-        sectionTitle.scrollIntoView({
-            block: 'start',
-            behavior: 'smooth'
-        })
-    }
-}
-let news = document.getElementById('news')
-if (news) {
-    news.addEventListener('click',toElementFunc)
-}
-function toElementFunc() {
-    let sectionTitle = document.querySelector('.section2__container');
-    if (sectionTitle) {
-        sectionTitle.scrollIntoView({
-            block: 'start',
-            behavior: 'smooth'
-        })
-    }
-}
-let contact = document.getElementById('contact')
-if (contact) {
-    contact.addEventListener('click',scrollFunc)
-}
-function scrollFunc() {
-    let sectionTitle = document.querySelector('.section4__container');
-    if (sectionTitle) {
-        sectionTitle.scrollIntoView({
-            block: 'start',
-            behavior: 'smooth'
-        })
-    }
-}
-//
-//
-// $(function() {
-//     $('.section2__wrapper').slick({
-//         infinite: true,
-//         arrows: false,
-//         dots: true,
-//         slidesToShow: 3,
-//         slidesToScroll: 3,
-//
-//     })
-// })
-//
-/*function initMap() {
-    // Координати центру карти
-    const center = { lat: 49.842957, lng: 24.031111 };
 
-    // Створення карти
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: center,
-      zoom: 12,
-    });
+//Form of validation
 
-    // Створення кастомного маркера
-    const marker = new google.maps.Marker({
-      position: center,
-      map: map,
-      icon: "path/to/custom-marker.png", // Шлях до зображення кастомного маркера
-      draggable: true, // Якщо ви хочете, щоб маркер був пересуваним
-    });
-  }
-
-//   const image = document.querySelector('.section3__wrapper_one');
-//
-//   image.addEventListener('mouseenter', function() {
-//     image.classList.add('zoomed');
-//   });
-//
-//   image.addEventListener('mouseleave', function() {
-//     image.classList.remove('zoomed');
-//   });
-//
-// /*
-// const icon = document.querySelector('.header__scroll_down');
-// icon.addEventListener('click', smoothScroll);
-//
-// function smoothScroll() {
-//     const targetElement = document.querySelector('.section1__container');
-//     targetElement.scrollIntoView({ behavior: 'smooth' });
-//   }
-//
-// setInterval(function () {
-//     $('.slickPrev').slick("slickPrev");
-// }, 2000);*/
-function initMap() {
-    new google.maps.Map(document.getElementById("map"), {
-      mapId: "8e0a97af9386fef",
-      center: { lat: 49.842957, lng: 24.031111 },
-      zoom: 12,
-    });
-  }
-
-  window.initMap = initMap;
-  
+$(document).ready(function() {
+  $("#form").validate({
+    rules: {
+      name : {
+        required: true,
+        minlength: 3
+      },
+      email: {
+        required: true,
+        email: true
+      },
+    }
+  });
+});
